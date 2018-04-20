5fd0cf6df0c6c68306fb3c1fba79baf359e71841
Si-Yang <cameraman03@hotmail.com>
2018-04-19 17:00:35 -0700

    w/e

--- NotePad/Controllers/DisplayNoteViewController.swift
+++ NotePad/Controllers/DisplayNoteViewController.swift
@@ -34,6 +34,9 @@ class DisplayNoteViewController: UIViewController {
         contentTextView!.layer.borderWidth = 3
         contentTextView!.layer.borderColor = UIColor.black.cgColor
         
+//        var lines:UnderlinedTextView!
+//        lines.draw(CGRect(0, 0, width: 1, height: 1))
+        
         // check note for an existing note
         if let note = note {
             // populates the text field & text view w/ the content of existing note
@@ -87,3 +90,36 @@ class DisplayNoteViewController: UIViewController {
         logInfo { "Leaving \(#function)" }
     }
 }
+
+class UnderlinedTextView: UITextView {
+    var lineHeight: CGFloat = 13.8
+    
+    override var font: UIFont? {
+        didSet {
+            if let newFont = font {
+                lineHeight = newFont.lineHeight
+            }
+        }
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        
+        let numberOfLines = Int(rect.height / lineHeight)
+        let topInset = textContainerInset.top
+        
+        for i in 1...numberOfLines {
+            let y = topInset + CGFloat(i) * lineHeight
+            
+            let line = CGMutablePath()
+            line.move(to: CGPoint(x: 0.0, y: y))
+            line.addLine(to: CGPoint(x: rect.width, y: y))
+            ctx?.addPath(line)
+        }
+        
+        ctx?.strokePath()
+        
+        super.draw(rect)
+    }
+}
+
